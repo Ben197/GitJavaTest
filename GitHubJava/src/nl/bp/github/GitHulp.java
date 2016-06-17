555fafeb153282de9e0d81fec19372ed7f9c1f11
Ben197 <jabro888uc@hotmail.com>
2016-06-17 20:55:42 +0200

    Initiele check in.

---
+++ GitHubJava/src/nl/bp/github/GitHulp.java
@@ -0,0 +1,10 @@
+package nl.bp.github;
+
+public class GitHulp {
+	
+	public void printBericht()
+	{
+		System.out.println("bericht in hulptest");
+	}
+
+}
---
+++ GitHubJava/src/nl/bp/github/GitStart.java
@@ -0,0 +1,22 @@
+package nl.bp.github;
+
+public class GitStart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GitStart gt = new GitStart();
+		GitHulp gh = new GitHulp();
+		
+		
+		gt.printBericht2();
+		gh.printBericht();
+		
+		
+	}
+	
+	public void printBericht2()
+	{
+		System.out.println("bericht in starttest");
+	}
+
+}
